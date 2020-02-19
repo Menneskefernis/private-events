bd196ff6053afefc29e9a6e940c37a3d5c59d432
Henrik Nørgaard <menneskefernis@gmail.com>
2020-02-19 23:17:48 +0100

    Make user signup possible

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+
+    end
   end
 
   def show
   end
+
+  private
+    def user_params  
+      params.require(:user).permit(:name, :email)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  before_save :downcase_email
+
+  private
+    def downcase_email
+      email.downcase!
+    end
 end
