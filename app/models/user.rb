9c16135b9d8d15ef8c775b421725dcf36f6ba940
Henrik Nørgaard <menneskefernis@gmail.com>
2020-02-23 18:57:06 +0100

    Sort out user - event association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,7 @@ class User < ApplicationRecord
   validates :name,  presence: true
   validates :email, presence: true
 
-  has_many :events, dependent: :destroy,
-                    foreign_key: :creator_id
-
-  def events
-    #events
-  end
+  has_many :events, dependent: :destroy, foreign_key: :creator_id
 
   private
     def downcase_email
